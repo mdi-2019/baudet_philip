86c4772fc430e83f82e68684a4c0f34cef8bc239
Clément BAUDET <clement.baudet@etudiant.univ-rennes1.fr>
2019-03-05 17:04:05 +0100

    add some comments

--- src/main/java/com/clemkev/calculator/Calculator.java
+++ src/main/java/com/clemkev/calculator/Calculator.java
@@ -8,17 +8,36 @@ abstract public class Calculator {
         this.lastRes = 0.0;
     }
 
+    /**
+     * @return Last value calculated
+     */
     public Double getLastRes() {
         return lastRes;
     }
 
+    /**
+     * @param lastRes Set the last value calculated
+     */
     public void setLastRes(Double lastRes) {
         this.lastRes = lastRes;
     }
 
+    /**
+     * @param cmd
+     * @return The commands list contains cmd
+     */
     public abstract Boolean isValid(String cmd);
 
+    /**
+     * Execute the command cmd with param as parameter
+     * @param cmd
+     * @param param
+     */
     public abstract void exec(String cmd, Double param);
 
+    /**
+     * Return the list of commands
+     * @return
+     */
     public abstract String getCommands();
 }
